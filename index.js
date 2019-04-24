ae0b225307895607b4274a635ffd3dbf121ec0a7
oli-gra <oliver.grant@gmail.com>
2019-04-24 13:58:16 +0100

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 function fetchBooks() {
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => renderBooks(json))
+}
+
+function fetchCharacters() {
+  fetch('https://anapioficeandfire.com/api/characters')
+  .then(resp => resp.json())
+  .then(json => renderCharacter(json))
+}
+
+function renderCharacter(json) {
+  console.log(json)
 }
 
 function renderBooks(json) {
+  console.log(json)
   const main = document.querySelector('main')
   json.forEach(book => {
-    const h2 = document.createElement('h2')
-    h2.innerHTML = `<h2>${book.name}</h2>`
-    main.appendChild(h2)
+    const h3 = document.createElement('h3')
+    h3.innerHTML = `<h3>${book.name}</h3>`
+    main.appendChild(h3)
   })
+  console.log(json[4].name)
 }
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks()
+  fetchCharacters()
 })
